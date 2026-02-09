95fe35f8d979d778cbeb70269f3f82999a42609e
pasakorn.m <pasakorn.m@bemplc.co.th>
2026-02-09 13:34:23 +0700

    fix error

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -4,15 +4,17 @@ class Settings(BaseSettings):
     database_url: str = "postgresql+psycopg2://alpr:alpr@localhost:5432/alpr"
     redis_url: str = "redis://localhost:6379/0"
     storage_dir: str = "./storage"
-    cors_origins: str = (
+    cors_origins: list[str] = [
             "http://localhost:5173",
             "http://10.32.70.136",
             "http://10.32.70.136:5173"
-            )
+            ]
 
     @property
     def cors_origins_list(self):
-        return [x.strip() for x in self.cors_origins.split(",") if x.strip()]
+        if isinstance(self.cors_origins, str):
+            return [x.strip() for x in self.cors_origins.split(",") if x.strip()]
+        return list(self.cors_origins)
 
     class Config:
         env_prefix = ""
